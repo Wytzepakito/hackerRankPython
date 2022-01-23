155c6a068ad491add27d230142906ffb59b89ec8
Wytzepakito <wytzepakito@gmail.com>
2022-01-23 22:55:00 +0100

    Take more time to think about edge cases before submitting

---
+++ sherlock_and_the_valid_string.py
@@ -0,0 +1,41 @@
+
+
+
+def isValid(s):
+    # Write your code here
+
+    dict = {}
+
+    for char in s:
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    print(dict)
+
+    l1 = list(dict.values())
+    set_len = len(set(l1))
+    if set_len == 1:
+        return("YES")
+
+    if set_len ==2:
+        maxi = max(set(l1))
+        mini = min(set(l1))
+        if maxi - mini == 1:
+            if l1.count(maxi) ==1:
+                return("YES")
+            if l1.count(mini) ==1:
+                return("YES")
+        if mini ==1 and l1.count(mini) == 1:
+            return("YES")
+    return("NO")
+
+
+print(isValid("abc"))
+print(isValid("abcc"))
+print(isValid("abccc"))
+
+
+
+print(isValid("ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"))
+
